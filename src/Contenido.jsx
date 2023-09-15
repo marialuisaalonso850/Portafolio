92d483da5a6832503ba3a0e4eed4066e51955980
marialuisaalonso850 <marialuisaalonso850@gmail.com>
2023-09-14 22:50:39 -0500

    actualizar7

--- src/Contenido.jsx
+++ src/Contenido.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import FirstContenido from './FirstContenido'
 import SecondContenido from './components/SecondContenido'
 import { Footer } from './Footer'
+import './index.css'
 
 const Contenido = () => {
   return (
