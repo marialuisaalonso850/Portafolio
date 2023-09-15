85a068305d4377bf4a72e5e5facf719647517ccb
marialuisaalonso850 <marialuisaalonso850@gmail.com>
2023-09-15 06:16:33 -0500

    actualizar10

--- src/FirstContenido.jsx
+++ src/FirstContenido.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FirstContenido = () => {
   return (
     <div>
-       <img className='mx-96 w-80' src="/public/img/luisa.png" alt="" id='imgLuisa' />
+       <img className='mx-96 w-80' src="../public/img/luisa.png" alt="" id='imgLuisa' />
     </div>
   )
 }
